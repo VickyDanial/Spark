5b1f3d6377dce8289c6ece11bf43bbbc94a9a155
VickyDanial <vickydaniel_sabbithi@yahoo.com>
2022-04-19 00:06:14 +0530

    added alternative solutions

--- spark_read_csv.py
+++ spark_read_csv.py
@@ -1,5 +1,14 @@
 spark = SparkSession.builder().master("local[1]")
           .appName("SparkByExamples.com")
           .getOrCreate()
+		  
 df = spark.read.csv("/tmp/resources/zipcodes.csv")
 df.printSchema()
+
+#alternative-1
+df1 = spark.read.format("csv").load("/tmp/resources/zipcodes.csv")
+df1.printSchema()
+
+#alternative-2
+df2 = spark.read.format("org.apache.spark.sql.csv").load("/tmp/resources/zipcodes.csv")
+df2.printSchema()
